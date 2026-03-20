01190d19f9f39afd80ea13aabd723d7191cc285e
roaming <wilhelmus4321@gmail.com>
2026-03-20 19:59:14 +0000

    Add package.nix for nixpkgs submission
    - Use fetchFromGitHub to fetch from v0.1.0 release
    - Correct hash: sha256-XtGmw6cDx7QQO8s0fXRsQKKM7oqRWwNzqCPoIbRTxdU=
    - Tested and builds successfully from GitHub tarball
    - Ready to copy to pkgs/by-name/do/dotman/ in nixpkgs

---
+++ package.nix
@@ -0,0 +1,49 @@
+{ lib
+, stdenv
+, fetchFromGitHub
+, cmake
+, ncurses
+}:
+
+stdenv.mkDerivation rec {
+  pname = "dotman";
+  version = "0.1.0";
+
+  src = fetchFromGitHub {
+    owner = "roamingparrot";
+    repo = "nixos-config-manager";
+    rev = "v${version}";
+    hash = "sha256-XtGmw6cDx7QQO8s0fXRsQKKM7oqRWwNzqCPoIbRTxdU=";
+  };
+
+  nativeBuildInputs = [ cmake ];
+  buildInputs = [ ncurses ];
+
+  preConfigure = ''
+    cd src
+  '';
+
+  cmakeFlags = [
+    "-DCMAKE_BUILD_TYPE=Release"
+  ];
+
+  installPhase = ''
+    mkdir -p $out/bin
+    cp dotman $out/bin/
+  '';
+
+  meta = with lib; {
+    description = "A TUI for managing NixOS packages";
+    longDescription = ''
+      dotman is a terminal-based user interface for searching, installing,
+      and removing NixOS packages by directly editing your Nix configuration files.
+      It provides an intuitive way to manage your system packages without
+      manually editing configuration files.
+    '';
+    homepage = "https://github.com/roamingparrot/nixos-config-manager";
+    license = licenses.mit;
+    maintainers = [ ];
+    mainProgram = "dotman";
+    platforms = platforms.linux;
+  };
+}
